8b6a742a892dc9ba9cd440bec95b0155b07b8fcf
tiagovla <tiagovla@gmail.com>
2022-07-22 21:29:05 -0300

    feat: add extension field to translators

--- lua/zotex/translators/betterbibtex.lua
+++ lua/zotex/translators/betterbibtex.lua
@@ -1,1 +1,10 @@
 --TODO Implement this
+local M = {}
+
+function M.citekey(item) end
+
+function M.do_export(item) end
+
+M.extension = ""
+
+return M
--- lua/zotex/translators/bibtex.lua
+++ lua/zotex/translators/bibtex.lua
@@ -1,6 +1,6 @@
 local M = {}
 
-local fieldMap = {
+local field_map = {
     address = "place",
     chapter = "section",
     edition = "edition",
@@ -19,6 +19,7 @@ local fieldMap = {
     language = "language",
     assignee = "assignee",
 }
+
 local months = {
     "jan",
     "feb",
@@ -34,7 +35,7 @@ local months = {
     "dec",
 }
 
-local zotero2bibtexTypeMap = setmetatable({
+local zotero2bibtex_type_map = setmetatable({
     book = "book",
     bookSection = "incollection",
     journalArticle = "article",
@@ -56,7 +57,7 @@ local zotero2bibtexTypeMap = setmetatable({
     end,
 })
 
-local function writeField(field, value, isMacro)
+local function write_field(field, value, isMacro)
     local output = ""
     if value == nil or value == "" then
         return output
@@ -72,7 +73,7 @@ local function writeField(field, value, isMacro)
     return output
 end
 
-M.types = vim.tbl_keys(zotero2bibtexTypeMap)
+M.types = vim.tbl_keys(zotero2bibtex_type_map)
 function M.citekey(item)
     local output, count = "", 0
     for _, creator in pairs(item.creators) do
@@ -95,27 +96,27 @@ end
 
 function M.do_export(item)
     local output = ""
-    local type = zotero2bibtexTypeMap[item.itemType]
-    for k, v in pairs(fieldMap) do
-        output = output .. writeField(k, item[v])
+    local type = zotero2bibtex_type_map[item.itemType]
+    for k, v in pairs(field_map) do
+        output = output .. write_field(k, item[v])
     end
     local number = item.reportNumber or item.issue or item.seriesNumber or item.parentNumber
-    output = output .. writeField("number", number)
-    output = output .. writeField("urldate", item.accessDate)
+    output = output .. write_field("number", number)
+    output = output .. write_field("urldate", item.accessDate)
     if item.publicationTitle then
         if item.itemType == "bookSection" or item.itemType == "conferencePaper" then
-            output = output .. writeField("booktitle", item.publicationTitle)
+            output = output .. write_field("booktitle", item.publicationTitle)
         else
-            output = output .. writeField("journal", item.publicationTitle)
+            output = output .. write_field("journal", item.publicationTitle)
         end
     end
     if item.publisher then
         if item.itemType == "thesis" then
-            output = output .. writeField("school", item.publisher)
+            output = output .. write_field("school", item.publisher)
         elseif item.itemType == "report" then
-            output = output .. writeField("institution", item.publisher)
+            output = output .. write_field("institution", item.publisher)
         else
-            output = output .. writeField("publisher", item.publisher)
+            output = output .. write_field("publisher", item.publisher)
         end
     end
     local authors, editors, translators, collaborators = {}, {}, {}, {}
@@ -144,19 +145,19 @@ function M.do_export(item)
 
     if #authors > 0 then
         local text = table.concat(authors, " and ")
-        output = output .. writeField("author", "{" .. text .. "}", true)
+        output = output .. write_field("author", "{" .. text .. "}", true)
     end
     if #editors > 0 then
         local text = table.concat(editors, " and ")
-        output = output .. writeField("editor", "{" .. text .. "}", true)
+        output = output .. write_field("editor", "{" .. text .. "}", true)
     end
     if #translators > 0 then
         local text = table.concat(translators, " and ")
-        output = output .. writeField("translator", "{" .. text .. "}", true)
+        output = output .. write_field("translator", "{" .. text .. "}", true)
     end
     if #collaborators > 0 then
         local text = table.concat(collaborators, " and ")
-        output = output .. writeField("collaborator", "{" .. text .. "}", true)
+        output = output .. write_field("collaborator", "{" .. text .. "}", true)
     end
 
     local citekey = M.citekey(item)
@@ -164,14 +165,16 @@ function M.do_export(item)
     if item.date then
         local date = {}
         date.year = string.match(item.date, "%d%d%d%d")
-        output = output .. writeField("year", date.year)
+        output = output .. write_field("year", date.year)
     end
     if item.pages then
         local pages = string.gsub(item.pages, "[-]+", "--")
-        output = output .. writeField("pages", pages)
+        output = output .. write_field("pages", pages)
     end
     output = output .. ",\n}"
     return output
 end
+
+M.extension = ".bib"
 
 return M
