42b804c44f6387297b1528084a764d5a8eba4947
tiagovla <tiagovla@gmail.com>
2023-02-04 00:17:26 -0300

    feat: update bibtex translator to match zotero's citekey

--- lua/zotex/translators/bibtex.lua
+++ lua/zotex/translators/bibtex.lua
@@ -1,20 +1,22 @@
 local M = {}
 
+local cite_key_format = "%a_%t_%y"
+
 local field_map = {
+    title = "title",
+    volume = "volume",
+    issn = "ISSN",
+    doi = "DOI",
+    abstract = "abstractNote",
     address = "place",
     chapter = "section",
     edition = "edition",
     type = "type",
     series = "series",
-    title = "title",
-    volume = "volume",
     copyright = "rights",
     isbn = "ISBN",
-    issn = "ISSN",
     shorttitle = "shortTitle",
     url = "url",
-    doi = "DOI",
-    abstract = "abstractNote",
     nationality = "country",
     language = "language",
     assignee = "assignee",
@@ -34,6 +36,64 @@ local months = {
     "nov",
     "dec",
 }
+
+local always_map = {
+    ["|"] = "{\\textbar}",
+    ["<"] = "{\\textless}",
+    [">"] = "{\\textgreater}",
+    ["~"] = "{\\textasciitilde}",
+    ["^"] = "{\\textasciicircum}",
+    ["\\"] = "{\\textbackslash}",
+    ["{"] = "{\\{\\vphantom{\\}}}",
+    ["}"] = "{\\vphantom{\\{}\\}}",
+}
+
+local cite_key_title_banned_re = {
+    ["on"] = "",
+    ["an"] = "",
+    ["the"] = "",
+    ["some"] = "",
+    ["from"] = "",
+    ["in"] = "",
+    ["to"] = "",
+    ["of"] = "",
+    ["do"] = "",
+    ["with"] = "",
+    ["der"] = "",
+    ["die"] = "",
+    ["das"] = "",
+    ["ein"] = "",
+    ["eine"] = "",
+    ["einer"] = "",
+    ["eines"] = "",
+    ["einem"] = "",
+    ["einen"] = "",
+    ["un"] = "",
+    ["une"] = "",
+    ["la"] = "",
+    ["le"] = "",
+    ["l'"] = "",
+    ["el"] = "",
+    ["las"] = "",
+    ["los"] = "",
+    ["al"] = "",
+    ["uno"] = "",
+    ["una"] = "",
+    ["unos"] = "",
+    ["unas"] = "",
+    ["de"] = "",
+    ["des"] = "",
+    ["del"] = "",
+    ["d'"] = "",
+}
+
+local function escape_special_characters(str)
+    local new_str = str:gsub("[|<>~^\\{}]", function(c)
+        return always_map[c]
+    end):gsub("[#%$&_]", "\\%1")
+    new_str = new_str:gsub("\\vphantom{\\}}(.-)\\vphantom{\\{}", "%1")
+    return new_str
+end
 
 local zotero2bibtex_type_map = setmetatable({
     book = "book",
@@ -57,41 +117,143 @@ local zotero2bibtex_type_map = setmetatable({
     end,
 })
 
-local function write_field(field, value, isMacro)
+local function map_HTML_markup(characters)
+    characters = characters
+        :gsub("{\\textless}i{\\textgreater}(.+?){\\textless}/i{\\textgreater}", "\\textit{%1}")
+        :gsub("{\\textless}b{\\textgreater}(.+?){\\textless}/b{\\textgreater}", "\\textbf{%1}")
+        :gsub("{\\textless}sup{\\textgreater}(.+?){\\textless}/sup{\\textgreater}", "$^{\\textrm{%1}}$")
+        :gsub("{\\textless}sub{\\textgreater}(.+?){\\textless}/sub{\\textgreater}", "$_{\\textrm{%1}}$")
+        :gsub(
+            '{\\textless}span\\sstyle="small%-caps"{\\textgreater}(.+?){\\textless}/span{\\textgreater}',
+            "\\textsc{%1}"
+        )
+        :gsub("{\\textless}sc{\\textgreater}(.+?){\\textless}/sc{\\textgreater}", "\\textsc{%1}")
+    return characters
+end
+
+local function write_field(field, value, is_macro)
     local output = ""
     if value == nil or value == "" then
         return output
     end
+    if not is_macro and not (field == "url" or field == "doi" or field == "file" or field == "lccn") then
+        value = escape_special_characters(value)
+    end
+    if field ~= "url" and field ~= "doi" and field ~= "file" then
+        value = map_HTML_markup(value)
+    end
     output = output .. ",\n    " .. field .. " = "
-    if not isMacro then
+    if not is_macro then
         output = output .. "{"
     end
     output = output .. value
-    if not isMacro then
+    if not is_macro then
         output = output .. "}"
     end
     return output
 end
 
+local function parse_extra_fields(extra)
+    local lines = extra:gmatch "[^\r\n]+"
+    local fields = {}
+    for line in lines do
+        local rec = { raw = line }
+        line = line:match "^%s*(.-)%s*$"
+        local splitAt = line:find ":"
+        if splitAt and splitAt > 1 then
+            rec.field = line:sub(1, splitAt - 1):match "^%s*(.-)%s*$"
+            rec.value = line:sub(splitAt + 1):match "^%s*(.-)%s*$"
+        end
+        table.insert(fields, rec)
+    end
+    return fields
+end
+
+local function tidy_accents(s)
+    local r = string.lower(s)
+    r = string.gsub(r, "[ä]", "ae")
+    r = string.gsub(r, "[ö]", "oe")
+    r = string.gsub(r, "[ü]", "ue")
+    r = string.gsub(r, "[àáâãå]", "a")
+    r = string.gsub(r, "æ", "ae")
+    r = string.gsub(r, "ç", "c")
+    r = string.gsub(r, "[èéêë]", "e")
+    r = string.gsub(r, "[ìíîï]", "i")
+    r = string.gsub(r, "ñ", "n")
+    r = string.gsub(r, "[òóôõ]", "o")
+    r = string.gsub(r, "œ", "oe")
+    r = string.gsub(r, "[ùúû]", "u")
+    r = string.gsub(r, "[ýÿ]", "y")
+    return r
+end
+
 M.types = vim.tbl_keys(zotero2bibtex_type_map)
-function M.citekey(item)
-    local output, count = "", 0
-    for _, creator in pairs(item.creators) do
-        if creator.name then
-            output = output .. creator.name
-        else
-            output = output .. creator.lastName
+
+local cite_key_conversions = {
+    a = function(flags, item)
+        if item.creators and item.creators[1] and item.creators[1].lastName then
+            return string.lower(item.creators[1].lastName:gsub(" ", "_"):gsub(",", ""))
         end
-        count = count + 1
-        if count == 2 then
-            break
+        return "noauthor"
+    end,
+
+    t = function(flags, item)
+        if item.title then
+            local title = item.title:lower():gsub("%S+", cite_key_title_banned_re)
+            return string.match(title, "%w+") or ""
+        end
+        return "notitle"
+    end,
+
+    y = function(flags, item)
+        if item.date then
+            local year = string.match(item.date, "^[0-9]+")
+            if year then
+                return year
+            end
+        end
+        return "nodate"
+    end,
+}
+
+function M.citekey(item, extra_fields, citekeys)
+    if extra_fields then
+        for i, field in ipairs(extra_fields) do
+            if field.field and field.value and string.lower(field.field) == "citation key" then
+                return table.remove(extra_fields, i).value
+            end
         end
     end
-    if item.date then
-        local year = string.match(item.date, "%d%d%d%d") or ""
-        output = output .. year
+    if item.citationKey then
+        return item.citationKey
     end
-    return output:gsub("%s+", "")
+    local basekey = ""
+    local ck_remaining = cite_key_format
+    while ck_remaining:match "%%(%w)" do
+        local s, e, r = ck_remaining:find "%%(%w)"
+        basekey = basekey .. ck_remaining:sub(0, s - 1 or 0)
+        local flags = ""
+        local f = cite_key_conversions[r]
+        if type(f) == "function" then
+            local value = f(flags, item)
+            basekey = basekey .. value
+        end
+        ck_remaining = ck_remaining:sub(e + 1)
+    end
+    if ck_remaining:len() > 0 then
+        basekey = basekey .. ck_remaining
+    end
+    basekey = tidy_accents(basekey)
+    basekey = basekey:gsub("[^a-z0-9!$&*+-./:;<>?[]^_`|]+", "")
+    local citekey = basekey
+    local i = 0
+    citekeys = citekeys or {}
+    while citekeys[citekey] do
+        i = i + 1
+        citekey = basekey .. "_" .. i
+    end
+    citekeys[citekey] = true
+    return citekey
 end
 
 function M.do_export(item)
@@ -121,25 +283,29 @@ function M.do_export(item)
     end
     local authors, editors, translators, collaborators = {}, {}, {}, {}
 
-    for _, creator in pairs(item.creators) do
-        local creatorString = ""
-        if creator.name then
-            creatorString = creatorString .. creator.name
-        else
-            creatorString = creator.lastName .. ", " .. creator.firstName
-        end
-        if creator.fieldMode then
-            creatorString = "{" .. creatorString .. "}"
-        end
+    if item.creators then
+        for _, creator in pairs(item.creators) do
+            local creatorString = ""
+            if creator.name then
+                creatorString = creatorString .. creator.name
+            else
+                creatorString = creator.lastName .. ", " .. creator.firstName
+            end
+            if creator.fieldMode then
+                creatorString = "{" .. creatorString .. "}"
+            end
 
-        if creator.creatorType == "editor" or creator.creatorType == "seriesEditor" then
-            table.insert(editors, creatorString)
-        elseif creator.creatorType == "translator" then
-            table.insert(translators, creatorString)
-        elseif creator.creatorType == "author" then
-            table.insert(authors, creatorString)
-        else
-            table.insert(collaborators, creatorString)
+            creatorString = escape_special_characters(creatorString)
+
+            if creator.creatorType == "editor" or creator.creatorType == "seriesEditor" then
+                table.insert(editors, creatorString)
+            elseif creator.creatorType == "translator" then
+                table.insert(translators, creatorString)
+            elseif creator.creatorType == "author" then
+                table.insert(authors, creatorString)
+            else
+                table.insert(collaborators, creatorString)
+            end
         end
     end
 
@@ -165,7 +331,19 @@ function M.do_export(item)
     if item.date then
         local date = {}
         date.year = string.match(item.date, "%d%d%d%d")
+        date.month = string.match(item.date, "%d%d%d%d%-?(%d%d)")
+        if date.month and tonumber(date.month) > 0 then
+            local month = months[tonumber(date.month)]
+            output = output .. write_field("month", month, true)
+        end
         output = output .. write_field("year", date.year)
+    end
+    if item.tags and #item.tags > 0 then
+        local tagString = ""
+        for _, tag in ipairs(item.tags) do
+            tagString = tagString .. ", " .. tag.tag
+        end
+        write_field("keywords", string.sub(tagString, 3))
     end
     if item.pages then
         local pages = string.gsub(item.pages, "[-]+", "--")
